acdbc285135dd5172e5aeb02403efe84bad66022
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-05-16 20:15:34 +0300

    second

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    SECRET_KEY='7905'
-    # SQLALCHEMY_DATABASE_URI = 'postgres://kzftmbyeacyolt:934aee6a457a0561bbf0a00cf5bfd0deb8bd65f5d5deefcb5ba19b8887774811@ec2-34-236-94-53.compute-1.amazonaws.com:5432/ddvq3evhnj0fum'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:just@localhost/beem'
+    SECRET_KEY='885'
+    SQLALCHEMY_DATABASE_URI = 'postgres://kzftmbyeacyolt:934aee6a457a0561bbf0a00cf5bfd0deb8bd65f5d5deefcb5ba19b8887774811@ec2-34-236-94-53.compute-1.amazonaws.com:5432/ddvq3evhnj0fum'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:just@localhost/beem'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     # SQLALCHEMY_DATABASE_URI = 'postgres://mchocezjfxzarl:a9b935d4e036cca5080ed17a14a954ccd97d7ba3a13027f961e36188b8efebf2@ec2-34-236-94-53.compute-1.amazonaws.com:5432/d5gamob8slp5c4'
     
@@ -33,11 +33,11 @@ class ProdConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
     # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:just@localhost/beem'
-    # uri = os.getenv('DATABASE_URL')
-    # if uri and uri.startswith('postgres://'):
-    #  uri = uri.replace('postgres://', 'postgresql://', 1)
+    uri = os.getenv('DATABASE_URL')
+    if uri and uri.startswith('postgres://'):
+     uri = uri.replace('postgres://', 'postgresql://', 1)
         
-    # SQLALCHEMY_DATABASE_URI=uri 
+    SQLALCHEMY_DATABASE_URI=uri 
 
 
 class TestConfig(Config):
@@ -48,7 +48,7 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:just@localhost/beem'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:just@localhost/beem'
 
     DEBUG = True
 
